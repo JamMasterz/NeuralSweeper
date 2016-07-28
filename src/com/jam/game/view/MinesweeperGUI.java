97ff9b7111e8ce103e728522353ebef64ad34499
JamMasterz <michaelchonewicz@gmail.com>
2016-07-28 13:32:52 +0200

    Added a GUI class that will render the board

---
+++ src/com/jam/game/view/MinesweeperGUI.java
@@ -0,0 +1,14 @@
+package com.jam.game.view;
+
+import javax.swing.JPanel;
+
+import com.jam.game.model.Board;
+
+public class MinesweeperGUI {
+	private Board board;
+	private JPanel panel;
+	
+	public JPanel getGUI(){
+		return null;
+	}
+}
