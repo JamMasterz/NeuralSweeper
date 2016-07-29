4b1e891c517a91ae1c2339b40707fd10836c2631
JamMasterz <wawrzynchonewicz@gmail.com>
2016-07-29 19:12:52 +0200

    Some more work on the network

--- src/com/jam/neural/NodeLayer.java
+++ src/com/jam/neural/NodeLayer.java
@@ -5,7 +5,7 @@ public class NodeLayer {
 	private int numInputsPerNode;
 	
 	public NodeLayer(int numNodes, int numInputsPerNode){
-		if (numNodes == 0) throw new IllegalArgumentException("The number of nodes in a layer must be greater than 0");
+		if (numNodes < 1) throw new IllegalArgumentException("The number of nodes in a layer must be greater than 0");
 		this.numInputsPerNode = numInputsPerNode;
 		
 		for (int i = 0; i < numNodes; i++){
@@ -19,23 +19,43 @@ public class NodeLayer {
 	 * @param inputs Inputs
 	 * @return Output values from every node in this layer. Can be used as inputs to next layer
 	 */
-	protected float[] evaluateLayer(float[] inputs){
+	protected float[] evaluateLayer(float[] inputs, boolean binary){
 		if (numInputsPerNode != inputs.length) throw new IllegalArgumentException("The amount of inputs don't match this layers node configuration");
 		
 		float[] output = new float[nodes.length];
 		
 		for (int i = 0; i < nodes.length; i++){
-			output[i] = nodes[i].evaluateNode(inputs);
+			output[i] = nodes[i].evaluateNode(inputs, binary);
 		}
 		
 		return output;
+	}
+	
+	public int size(){
+		return nodes.length;
+	}
+	
+	protected float[] getLayerGenes(){
+		float[] layerGenes = new float[getGenesSize()];
+		
+		int nextIndex = 0;
+		for (int i = 0; i < layerGenes.length; i++){
+			float[] nodeGene = nodes[i].getGene();
+			System.arraycopy(nodeGene, 0, layerGenes, nextIndex, nodeGene.length);
+			nextIndex += nodeGene.length;
+		}
+		
+		return layerGenes;
+	}
+	
+	protected int getGenesSize(){
+		return nodes.length * (numInputsPerNode + 1);
 	}
 	
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
 		
-		builder.append("Layer: ");
 		for (NeuralNode node : nodes){
 			builder.append('\t');
 			builder.append(node.toString());
