e225c19584c9375ce8ba18011d13067f75f388b0
JamMasterz <michaelchonewicz@gmail.com>
2016-08-02 01:58:14 +0200

    Fixed the ArrayOutOfBounds in getCoveredFields

--- src/com/jam/game/controller/MainTest.java
+++ src/com/jam/game/controller/MainTest.java
@@ -9,14 +9,14 @@ import javax.swing.JPanel;
 public class MainTest {
 
 	public static void main(String[] args) {
-		Game[] games = new Game[9];
+		Game[] games = new Game[1];
 		for (int i = 0; i < games.length; i++){
 			games[i] = new Game(DefaultGamePreference.NOOB);
 		}
 		
 		JFrame frame = new JFrame("Minesweeper test");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setLayout(new GridLayout(3, 3));
+		frame.setLayout(new GridLayout(1, 1));
 		frame.setResizable(false);
 		for (int i = 0; i < games.length; i++) {
 			JPanel p = games[i].getGUI(1);
