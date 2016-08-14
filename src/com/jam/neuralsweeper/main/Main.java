c751c327469b1413e8c35984a3071326b01d41aa
JamMasterz <michaelchonewicz@gmail.com>
2016-08-14 22:13:10 +0200

    Literally just copied the code the a new identical file
    Apparently, that fixed all the compiler errors

--- src/com/jam/neuralsweeper/main/Main.java
+++ src/com/jam/neuralsweeper/main/Main.java
@@ -3,6 +3,7 @@ package com.jam.neuralsweeper.main;
 import com.jam.neural.controller.NeuralNetwork;
 
 public class Main {
+
 	public static void main(String[] args) {
 		new NeuralNetwork(new NeuralSweeperSetup());
 	}
